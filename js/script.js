9b861caf28ffff54953b2252ef442e3596a38410
noormohammad20 <mdsaim204@gmail.com>
2022-02-28 20:57:29 +0600

    load data form api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('hello');
+// load mobile data 
+const loadMobilesData = () => {
+    const textField = document.getElementById('text-field')
+    const textFieldValue = textField.value
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${textFieldValue}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+    // clear textField
+    textField.value = ''
+}
+
+loadMobilesData()
+
