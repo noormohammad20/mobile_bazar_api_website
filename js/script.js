54548c1795d972d35fd86cb86dbc3a9cf324fe46
noormohammad20 <mdsaim204@gmail.com>
2022-02-28 21:25:21 +0600

    successfully show in display mobile data

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const loadMobilesData = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${textFieldValue}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayMobileData(data.data))
 
     // clear textField
     textField.value = ''
@@ -14,3 +14,25 @@ const loadMobilesData = () => {
 
 loadMobilesData()
 
+// display mobile data 
+
+const displayMobileData = (mobiles) => {
+    const mobileContainer = document.getElementById('mobile-container')
+    mobiles.forEach(mobile => {
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+    <div class="card h-100">
+                    <img src="${mobile.image}" class="card-img-top w-50" alt="...">
+                    <div class="card-body">
+                     <h5 class="card-title">${mobile.brand}</h5>
+                      <h6>${mobile.phone_name}</h6>
+                     <button class="btn btn-outline-info text-dark">See Details</button>
+                    </div>
+                </div>
+    `
+        mobileContainer.appendChild(div)
+
+    })
+
+}
