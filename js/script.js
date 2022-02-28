a133ab71befe00d1daae93a4a77f33d2ad9b3d82
noormohammad20 <mdsaim204@gmail.com>
2022-03-01 00:45:47 +0600

    display details done

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ const loadMobilesData = () => {
 }
 
 
-
 // display mobile data 
 
 const displayMobileData = (mobiles) => {
@@ -22,8 +21,7 @@ const displayMobileData = (mobiles) => {
     mobileContainer.textContent = ''
 
 
-    mobiles.slice(0, 20).forEach(mobile => {
-
+    mobiles?.slice(0, 20).forEach(mobile => {
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
@@ -51,7 +49,7 @@ const loadDetails = (mobileId) => {
         .then(res => res.json())
         .then(data => displayDetails(data))
 }
-// display ditails
+// display details
 
 const displayDetails = (details) => {
 
@@ -62,9 +60,15 @@ const displayDetails = (details) => {
             <div class="card h-100">
             <img src="${details.data.image}" class="card-img-top w-50" alt="...">
             <div class="card-body">
-            <h6 class="card-title">${details.data.releaseDate}</h6>
-            <p>${details.data.sensors}</P>
-           
+            <h6 class="card-title">${details.data.releaseDate ? details.data.releaseDate : 'no realise date found'}</h6>
+            <p>${details.data.mainFeatures.sensors}</P>
+
+            <p>${details.data.others.WLAN}</P>
+            <p>${details.data.others.Bluetooth}</P>
+            <p>${details.data.others.GPS}</P>
+            <p>${details.data.others.NFC}</P>
+            <p>${details.data.others.Radio}</P>
+            <p>${details.data.others.USB}</P>
            
             </div >
             </div >
